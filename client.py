a5f9f7fe7967348000912af3fd379c052543af79
fhernan22 <fhern103@fiu.edu>
2020-09-21 16:54:15 -0400

    Added LS command

--- client.py
+++ client.py
@@ -15,6 +15,7 @@ def main():
     try:
         sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
         sock.connect((SERVER,PORT))
+        addr, dataPort = sock.getsockname()
         response = sock.recv(1024)
         print(response)
 
@@ -29,6 +30,68 @@ def main():
 
         if int(response[0:3]) >= 300:
             sys.exit(1)
+
+        while True:
+            command = raw_input('myftp> ')
+
+            if command.upper() == 'LS':
+                dataPort += 1
+
+                isPortAvailable = False 
+
+                while isPortAvailable == False:
+                    try:
+                        dataSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+                        dataSocket.bind(('',dataPort))
+                        isPortAvailable = True
+
+                    except socket.error:
+                        dataPort += 1
+
+                # sock.send('PASV\r\n')
+                # response = sock.recv(1024)
+                # print(response)
+
+                # availabePorts = response[response.find('(') + 1: len(response) - 4].split(",")
+                # print(availabePorts)
+
+                splitAddress = str.split(addr, ".")
+                dataPortOne = str(int(hex(dataPort)[2:4], 16))
+                dataPortTwo = str(int(hex(dataPort)[4:6], 16))
+
+                message = 'PORT ' + splitAddress[0] \
+                                + "," + splitAddress[1] + "," + splitAddress[2] \
+                                + "," + splitAddress[3] + "," \
+                                + dataPortOne + ',' + dataPortTwo + '\r\n'
+
+                sock.send(message)
+                response = sock.recv(1024)
+                print(response)
+
+                message = 'NLST\r\n'
+                sock.send(message)
+
+                dataSocket.listen(1)
+                dataConn = dataSocket.accept()[0]
+
+                response = sock.recv(1024)
+                print(response)
+
+                listing = dataConn.recv(1024)
+                print(listing)
+                dataConn.close()
+
+                response = sock.recv(1024)
+                print(response)
+                dataSocket.close()
+
+            elif command.upper() == 'QUIT':
+                sock.send('QUIT\r\n')
+                response = sock.recv(1024)
+                print(response)
+                sock.close()
+                quit()
+                
 
     except socket.error as e:
         sys.stderr.write('ERROR: {}\n'.format(e))
